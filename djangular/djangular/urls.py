212b0c62e8d26a92f8b62caf6b1f845edb9d5c34
kranthikiran01 <guduru.kranthikiran@gmail.com>
2015-12-29 01:49:54 +0530

    Authentication feature added

--- djangular/djangular/urls.py
+++ djangular/djangular/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls import patterns, url, include
+from rest_framework_nested import routers
+from authentication.views import AccountViewSet
+from authentication.views import LoginView,LogoutView
 from djangular.views import IndexView
-from django.conf.urls import url
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
 
-
-urlpatterns = [
+urlpatterns = patterns(
+     '',
+    url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
+    url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
     url('^.*$', IndexView.as_view(), name='index'),
-]
+)
--- djangular/static/javascripts/djangular.js
+++ djangular/static/javascripts/djangular.js
@@ -1,2 +1,31 @@
+(function(){
 angular
-  .module('djangular', []);
+  .module('djangular', [
+    'djangular.config',
+    'djangular.routes',
+    'djangular.authentication',
+    'djangular.layout'
+  ]);
+
+angular
+  .module('djangular.routes',['ngRoute']);
+
+angular
+  .module('djangular.config',[]);
+
+  angular
+  .module('djangular')
+  .run(run);
+
+run.$inject = ['$http'];
+
+/**
+* @name run
+* @desc Update xsrf $http headers to align with Django's defaults
+*/
+function run($http) {
+  $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+  $http.defaults.xsrfCookieName = 'csrftoken';
+}
+
+})();
